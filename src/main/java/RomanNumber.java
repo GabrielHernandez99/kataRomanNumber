0c3d7a287fc303a7a416d21dee7862ac6b0a64e2
GabrielHernandez99 <gabriel090199@gmail.com>
2023-07-31 18:15:25 +0100

    refactor RomanNumber romanConverter sin else

--- src/main/java/RomanNumber.java
+++ src/main/java/RomanNumber.java
@@ -6,10 +6,7 @@ public class RomanNumber {
     public String romanConverter (int number){
         if(number==1){
             return "I";
-        } else{
-            return "II";
         }
-
-
+            return "II";
     }
 }
