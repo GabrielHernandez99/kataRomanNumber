9a8e7e71bd378a59625d1dc997b4dc6f1d304b45
GabrielHernandez99 <gabriel090199@gmail.com>
2023-07-31 18:09:41 +0100

    red RomanNumberTest pasar1ARomanos

---
+++ src/main/java/RomanNumber.java
@@ -0,0 +1,9 @@
+public class RomanNumber {
+
+    public RomanNumber() {
+    }
+
+    public String romanConverter (int number){
+        return "";
+    }
+}
---
+++ src/test/java/RomanNumberTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RomanNumberTest {
+    private RomanNumber romanNumber;
+
+    @BeforeEach
+    void setUp() {
+        romanNumber=new RomanNumber();
+    }
+    @Test
+    public void pasar1ARomanos(){
+        String numero=romanNumber.romanConverter(1);
+        assertEquals(numero, "I");
+    }
+}
